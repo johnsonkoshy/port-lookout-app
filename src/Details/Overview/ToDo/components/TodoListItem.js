f8c5d845102e5d632a98ae88216d206cc8bb8faf
lkumarcc <lkumarcc@gmail.com>
2022-07-19 12:16:54 -0400

    adding todo list stuff

--- src/Details/Overview/ToDo/components/TodoListItem.js
+++ src/Details/Overview/ToDo/components/TodoListItem.js
@@ -9,20 +9,65 @@ import {
   FormControlLabel
 } from "@mui/material";
 
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-const TodoListItem = React.memo(
-  ({ text, divider, checked, onCheckBoxToggle, onButtonClick }) => (
-    <ListItem divider={divider} sx={{p:0}}>
-      
-      <FormControlLabel control={<Checkbox onClick={onCheckBoxToggle} checked={checked} disableRipple />} label={text} />
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
-      
-      <ListItemSecondaryAction sx={{right:0}}>
-        <IconButton aria-label="Delete Todo" onClick={onButtonClick} >
-          <DeleteOutlineOutlinedIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PersonIcon from '@mui/icons-material/Person';
+
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+  const TodoListItem = React.memo(
+  ({ persontext, completetext, labeltext, divider, checked, onCheckBoxToggle, onButtonClick }) => 
+  
+ (
+  
+
+     
+      <Grid mt={1} mb={1}>
+         <Box sx={{border: 1, borderRadius: '10px', borderColor: 'grey.500'}}>
+      {/* <ListItem divider={divider} sx={{p:0}}> */}
+        {/* checkbox */}
+        <Grid container >
+          <Grid item xs={2} >
+            <FormControlLabel control={<Checkbox onClick={onCheckBoxToggle} checked={checked}  disableRipple />} />
+          </Grid>
+
+          <Grid item xs={8}>
+            <ListItemText primary = {labeltext}></ListItemText>
+          </Grid>
+            
+         
+              {/* trashcan */}
+            <Grid item xs={1}>
+              <IconButton aria-label="Delete Todo" onClick={onButtonClick} >
+                <DeleteOutlineOutlinedIcon />
+              </IconButton>
+            </Grid>
+         
+            
+        </Grid>
+
+        <Grid container >
+          <Grid item xs={2}></Grid>
+          <Grid item xs = {5}>
+          <ListItem>
+            <ListAltIcon fontSize="small" color="action" ></ListAltIcon> 
+            <ListItemText primary = {completetext} sx={{fontSize:'0.85em'}} disableTypography></ListItemText>
+            </ListItem>
+          </Grid>
+          <Grid item xs={5}>
+            <ListItem>
+            <PersonIcon fontSize="small" color="action"></PersonIcon>
+            <ListItemText  primary = {persontext} sx={{fontSize:'0.85em'}} disableTypography></ListItemText>
+            </ListItem>
+          </Grid>
+          
+        </Grid>
+      {/* </ListItem> */}
+      </Box>
+      </Grid>
+     
+
   )
 );
 
--- src/Details/Overview/ToDo/index.js
+++ src/Details/Overview/ToDo/index.js
@@ -4,11 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { useInputValue, useTodos } from "./custom-hooks";
+import {useState} from 'react';
 
 import Layout from "./components/Layout";
 
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
+import TodoListItem from "./components/TodoListItem";
 
 const TodoApp = React.memo(() => {
   const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
@@ -17,6 +19,17 @@ const TodoApp = React.memo(() => {
   const clearInputAndAddTodo = () => {
     clearInput();
     addTodo(inputValue);
+  };
+
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    
+    setIsActive(current => !current);
+  };
+
+  const secondarystyle = {
+    background: isActive ? 'purple' : '',
+    color: isActive ? 'white' : '',
   };
 
   return (
@@ -32,6 +45,41 @@ const TodoApp = React.memo(() => {
         onButtonClick={clearInputAndAddTodo}
         onInputKeyPress={(event) => keyInput(event, clearInputAndAddTodo)}
       />
+      <TodoListItem
+        sx = {{secondarystyle}}
+        onChange = {handleClick}
+        labeltext = "Bunker Planning (if needed)"
+        completetext = "Completed"
+        persontext = "John Doe"
+      ></TodoListItem>
+      <TodoListItem
+        sx = {{secondarystyle}}
+        onChange = {handleClick}
+        labeltext = "General Voyage Instruction (loading)"
+        completetext = "Completed"
+        persontext = "John Doe"
+      ></TodoListItem>
+      <TodoListItem
+        sx = {{secondarystyle}}
+        onChange = {handleClick}
+        labeltext = "Appoint Surveyor (Pre-Loading)"
+        completetext = "Completed"
+        persontext = "John Doe"
+      ></TodoListItem>
+      <TodoListItem
+        sx = {{secondarystyle}}
+        onChange = {handleClick}
+        labeltext = "Agency Nomination (loading)"
+        completetext = "Completed"
+        persontext = "John Doe"
+      ></TodoListItem>
+      <TodoListItem
+        sx = {{secondarystyle}}
+        onChange = {handleClick}
+        labeltext = "Proforma (Loading)"
+        completetext = "Completed"
+        persontext = "John Doe"
+      ></TodoListItem>
       
     </Layout>
   );
